6b4af79dacd41f3a99c289453d113e1592ba4717
IronLegends7519 <drafin@edenschool.fr>
2023-12-19 14:58:08 +0100

    debut js

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 import './style.css'
+
+const BtnFamily= document.querySelector("#BtnFamily")
+const BtnSpecial= document.querySelector("#BtnSpecial")
+const BtnSocial= document.querySelector("#BtnSocial")
+
+const Family=document.querySelector("#family-Gathering")
+const special= document.querySelector("#Special-Events")
+const social= document.querySelector("#Social-Events")
+BtnFamily.addEventListener('click',()=>{
+
+})
